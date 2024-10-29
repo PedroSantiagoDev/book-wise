1f3fb179e6e722551a473e702ea71ab460350198
Pedro Santiago <joaopedrosantiago1103@gmail.com>
2024-10-29 11:11:31 -0300

    feat: login auth

--- controllers/login.controller.php
+++ controllers/login.controller.php
@@ -2,4 +2,24 @@
 
 $mensagem = $_REQUEST['mensagem'] ?? '';
 
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+
+    $usuario = $database->query(
+        query: 'select * from usuarios where email = :email and senha = :senha',
+        params: [
+            ':email' => $email,
+            ':senha' => $senha
+        ]
+    )->fetch();
+
+    if ($usuario) {
+        $_SESSION['auth'] = $usuario;
+        $_SESSION['mensagem'] = 'Seja bem Vindo ' . $usuario['nome'] . '!';
+        header('location: /');
+        exit();
+    }
+}
+
 view('login', compact('mensagem'));
--- views/login.view.php
+++ views/login.view.php
@@ -1,14 +1,14 @@
 <div class="mt-6 grid grid-cols-2 gap-2">
     <div class="border border-stone-700 rounded">
         <h1 class="border-b border-stoner-700 text-stone-400 font-bold px-4 py-2">Login</h1>
-        <form class="p-4 space-y-4">
+        <form class="p-4 space-y-4" method="POST">
             <div class="flex flex-col">
                 <label class="text-stone-400 mb-1">Email</label>
                 <input
                     type="email"
                     class="borde-stone-800 border-2 rounded-md bg-stone-900 text-sm focus:outline-none px-2 py-1"
                     placeholder="Email"
-                    name="email" required>
+                    name="email">
             </div>
 
             <div class="flex flex-col">
@@ -17,7 +17,7 @@
                     type="password"
                     class="borde-stone-800 border-2 rounded-md bg-stone-900 text-sm focus:outline-none px-2 py-1"
                     placeholder="Senha"
-                    name="senha" required>
+                    name="senha">
             </div>
 
             <button
