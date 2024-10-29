33df5154484795b163c44a291ee19eb87cdf29f2
Pedro Santiago <joaopedrosantiago1103@gmail.com>
2024-10-29 10:37:05 -0300

    feat: add validacao

--- controllers/registrar.controller.php
+++ controllers/registrar.controller.php
@@ -1,10 +1,18 @@
 <?php
 
+require 'Validacao.php';
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $nome = $_POST['nome'];
-    $email = $_POST['email'];
-    $email_confirmacao = $_POST['email_confirmacao'];
-    $senha = $_POST['senha'];
+    $validacao = Validacao::validar([
+        'nome' => ['required'],
+        'email' => ['required', 'email', 'confirmed'],
+        'senha' => ['required', 'min:8', 'max:30', 'strong']
+    ], $_POST);
+
+    if ($validacao->naoPassou()) {
+        header('location: /login');
+        exit();
+    }
 
     $database->query(
         query: 'insert into usuarios (nome, email, senha) values (:nome, :email, :senha)',
--- views/login.view.php
+++ views/login.view.php
@@ -31,36 +31,49 @@
     <div class="border border-stone-700 rounded">
         <h1 class="border-b border-stoner-700 text-stone-400 font-bold px-4 py-2">Registro</h1>
         <form class="p-4 space-y-4" method="POST" action="/registrar">
-            <?php if (strlen($mensagem) > 0): ?>
+
+            <?php if (isset($mensagem) && strlen($mensagem)): ?>
                 <div class="border-green-800 bg-green-900 text-green-400 px-4 py-1 rounded-md border-2">
                     <?= $mensagem ?>
                 </div>
             <?php endif; ?>
+
+            <?php if (isset($_SESSION['validacoes']) && sizeof($_SESSION['validacoes'])): ?>
+                <div class="border-red-800 bg-red-900 text-red-400 px-4 py-1 rounded-md border-2">
+                    <ul>
+                        <li>Deu ruim!!</li>
+                    </ul>
+                    <?php foreach ($_SESSION['validacoes'] as $validacao): ?>
+                        <li><?= $validacao ?></li>
+                    <?php endforeach; ?>
+                </div>
+            <?php endif; ?>
+
             <div class="flex flex-col">
                 <label class="text-stone-400 mb-1">Nome</label>
                 <input
                     type="text"
                     class="borde-stone-800 border-2 rounded-md bg-stone-900 text-sm focus:outline-none px-2 py-1"
                     placeholder="Nome"
-                    name="nome" required>
+                    name="nome">
             </div>
 
             <div class="flex flex-col">
                 <label class="text-stone-400 mb-1">Email</label>
                 <input
-                    type="email"
+                    type="text"
                     class="borde-stone-800 border-2 rounded-md bg-stone-900 text-sm focus:outline-none px-2 py-1"
                     placeholder="Email"
-                    name="email" required>
+                    name="email">
             </div>
 
             <div class="flex flex-col">
                 <label class="text-stone-400 mb-1">Confirme seu Email</label>
                 <input
-                    type="email"
+                    type="text"
                     class="borde-stone-800 border-2 rounded-md bg-stone-900 text-sm focus:outline-none px-2 py-1"
                     placeholder="Email"
-                    name="email_confirmacao" required>
+                    name="email_confirmacao">
             </div>
 
             <div class="flex flex-col">
@@ -69,7 +82,7 @@
                     type="password"
                     class="borde-stone-800 border-2 rounded-md bg-stone-900 text-sm focus:outline-none px-2 py-1"
                     placeholder="Senha"
-                    name="senha" required>
+                    name="senha">
             </div>
 
             <button
